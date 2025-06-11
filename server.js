c02aa2c8a3a82bebf06e877dbd8888eb24ca043f
Edison25G <egunaucho1@espe.edu.ec>
2025-06-11 12:14:49 -0500

    Cambios realizados

--- server.js
+++ server.js
@@ -7,16 +7,27 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server)
 
-app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "index.html"))
-})
+// Servir el archivo index.html
+//app.get("/", (req, res) => {
+  //res.sendFile(join(__dirname, "index.html"))
+//})
+app.use(express.static(__dirname)) 
 
+// Manejar conexión de WebSockets
 io.on("connection", (socket) => {
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", msg)
-    })
+  console.log("Un usuario se conectó")
+
+  socket.on("chat message", (msg) => {
+    // msg es un objeto: { user, message }
+    io.emit("chat message", msg)
+  })
+
+  socket.on("disconnect", () => {
+    console.log("Un usuario se desconectó")
+  })
 })
 
-server.listen(4000, () => {
-    console.log("Server running on http://localhost:4000")
+// Iniciar el servidor
+server.listen(4000, '0.0.0.0', () => {
+  console.log("Server running on port 4000")
 })
